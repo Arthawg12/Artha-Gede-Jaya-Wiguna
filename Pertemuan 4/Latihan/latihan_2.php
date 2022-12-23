a9c32bd6beb1a52a58ef6a6df3964c420b2aff89
Arthawg12 <121308657+Arthawg12@users.noreply.github.com>
2022-12-24 00:01:28 +0700

    Add files via upload

---
+++ Pertemuan 4/Latihan/latihan_2.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+    <title> Pemrograman PHP dengan Array</title>
+    <head>
+    <body>
+    <?php
+     //penulisan array dapat dibuat seperti berikut 
+     $nama[] = "Gede";
+     $nama[] = "Arta";
+     $nama[] = "Jaya";
+     $nama[] = "Wiguna";
+     echo $nama[1] . $nama[2] . $nama[0] . $nama[3];
+     echo "<br>";
+     ?> 
+     </body>
+</html>
